25d4ea7f5784d11f4b441120eadc31d9bab4b158
DESARE GAGO <desaregago@DESAREs-MacBook-Pro.local>
2017-10-28 21:54:44 -0400

    create database

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Beacon Cheeseburger');
+
