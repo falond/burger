003a5c163c1905627c26f886a2ed27dd63ba3649
DESARE GAGO <desaregago@DESAREs-MacBook-Pro.local>
2017-11-01 07:00:48 -0400

    JAWDB_URL connection

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,29 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWDB_URL){
+  connection = mysql.createConnection(process.env.JAWDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
   database: "burger_db",
   port: 8889
 });
+};
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+//   console.log("connected as id " + connection.threadId);
+// });
 
-  console.log("connected as id " + connection.threadId);
-});
-
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
